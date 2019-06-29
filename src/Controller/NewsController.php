bf9848b8187d760852dd7cd9592c8c5c67c4372d
denis basov <basov@ag-media.ru>
2019-06-29 20:22:08 +0300

    Fix namespace for controllers.

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -1,6 +1,6 @@
 <?php
 
-    namespace App\Controller\Api;
+    namespace App\Controller;
 
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Request;
