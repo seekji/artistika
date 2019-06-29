a429ac5ecc7f9e379c0ad9b16bac0967e6226fdc
denis basov <basov@ag-media.ru>
2019-06-30 02:17:05 +0300

    task_25 fix namespace for subscribe.

--- src/Entity/Handbook/City.php
+++ src/Entity/Handbook/City.php
@@ -6,6 +6,7 @@ use Knp\DoctrineBehaviors\Model\Timestampable\Timestampable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Subscribe;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\Handbook\CityRepository")
