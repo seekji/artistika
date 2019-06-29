0619b205f148c0eb24d7422f619fd8194718bf51
denis basov <basov@ag-media.ru>
2019-06-30 02:13:51 +0300

    task_25 Update city namespace.

--- src/Entity/Subscribe.php
+++ src/Entity/Subscribe.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Knp\DoctrineBehaviors\Model\Timestampable\Timestampable;
+use App\Entity\Handbook\City;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\SubscribeRepository")
