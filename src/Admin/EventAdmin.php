5f8dba30363b92165318d9a89754da6f230fc38a
Denis basov <denibasov@yandex.ru>
2019-07-25 00:48:49 +0300

    task_1 Create `Event` entity.

---
+++ src/Admin/EventAdmin.php
@@ -0,0 +1,81 @@
+<?php
+
+    namespace App\Admin;
+
+    use Sonata\AdminBundle\Admin\AbstractAdmin;
+    use Sonata\AdminBundle\Datagrid\DatagridMapper;
+    use Sonata\AdminBundle\Datagrid\ListMapper;
+    use Sonata\AdminBundle\Form\FormMapper;
+    use Sonata\AdminBundle\Form\Type\ModelListType;
+    use Sonata\AdminBundle\Form\Type\ModelType;
+    use Sonata\AdminBundle\Show\ShowMapper;
+
+    /**
+     * Class HallAdmin
+     * @package App\Admin
+     */
+    class EventAdmin extends AbstractAdmin
+    {
+
+        /**
+         * @param DatagridMapper $filter
+         */
+        protected function configureDatagridFilters(DatagridMapper $filter)
+        {
+            $filter
+                ->add('id')
+                ->add('title')
+            ;
+        }
+
+        /**
+         * @param ListMapper $list
+         */
+        protected function configureListFields(ListMapper $list)
+        {
+            unset($this->listModes['mosaic']);
+
+            $list
+                ->add('id')
+                ->add('title')
+                ->add('city')
+                ->add('createdAt')
+                ->add('updatedAt')
+                ->add('_action', null, [
+                    'actions' => [
+                        'show'   => [],
+                        'edit'   => [],
+                        'delete' => [],
+                    ],
+                ]);
+        }
+
+        /**
+         * @param FormMapper $form
+         */
+        protected function configureFormFields(FormMapper $form)
+        {
+            $form
+                ->with('Свойства мероприятия')
+                    ->add('title')
+                    ->add('city',  ModelListType::class)
+                    ->add('hall', ModelListType::class)
+                    ->add('artists',  ModelType::class, ['multiple' => true])
+                ->end();
+        }
+
+        /**
+         * @param ShowMapper $showMapper
+         */
+        public function configureShowFields(ShowMapper $showMapper)
+        {
+            $showMapper
+                ->add('id')
+                ->add('title')
+                ->add('city')
+                ->add('hall')
+                ->add('createdAt')
+                ->add('updatedAt')
+            ;
+        }
+    }
