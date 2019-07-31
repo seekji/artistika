e506d5579c4fb744d678d63f523be2b2d5368a80
Denis basov <denibasov@yandex.ru>
2019-07-31 13:07:12 +0300

    task_35 Update news routes.

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -5,8 +5,32 @@
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Component\HttpFoundation\Response;
 
+    /**
+     * Class NewsController
+     * @package App\Controller
+     * @Route("/news")
+     */
     class NewsController extends AbstractController
     {
+        /**
+         * @Route("/", name="app.news.list")
+         *
+         * @return Response
+         */
+        public function list()
+        {
+            return $this->render('news/list.html.twig');
+        }
 
+        /**
+         * @Route("/{slug}/", name="app.news.show")
+         *
+         * @return Response
+         */
+        public function show()
+        {
+            return $this->render('news/index.html.twig');
+        }
     }
