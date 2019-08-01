e8d5ca8e4ae4a040d3789aa1f9aa60157d7f3640
Denis basov <denibasov@yandex.ru>
2019-08-02 00:45:07 +0300

    task_13 Create empty templates.

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,11 +2,33 @@
 
     namespace App\Controller;
 
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\HttpFoundation\Request;
+    use App\Entity\Event;
+    use App\Entity\Handbook\City;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
     use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
 
+    /**
+     * Class EventController
+     * @package App\Controller
+     * @Route("/event")
+     */
     class EventController extends AbstractController
     {
-
+        /**
+         * @param Event $event
+         * @param City $city
+         *
+         * @Route("/{city}/{event}/", name="app.event.show")
+         *
+         * @ParamConverter("city", options={"mapping": {"city": "slug"}})
+         * @ParamConverter("event", options={"mapping": {"event": "slug"}})
+         *
+         * @return Response
+         */
+        public function show(City $city, Event $event)
+        {
+            return $this->render('event/index.html.twig', ['event' => $event]);
+        }
     }
