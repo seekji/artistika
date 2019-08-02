6960f6d18cff94f459b464d5728790a01f1aee51
Denis basov <denibasov@yandex.ru>
2019-08-02 19:06:28 +0300

    task_13 Create ics rendering and route.

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,8 +3,10 @@
     namespace App\Controller;
 
     use App\Entity\Event;
+    use App\Entity\EventSchedule;
     use App\Entity\Handbook\City;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+    use Symfony\Component\HttpFoundation\ResponseHeaderBag;
     use Symfony\Component\Routing\Annotation\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
@@ -30,5 +32,37 @@
         public function show(City $city, Event $event)
         {
             return $this->render('event/index.html.twig', ['event' => $event]);
+        }
+
+        /**
+         * @Route("/calendar/{event_id}/{schedule_id}/")
+         *
+         * @param Event $event
+         * @param EventSchedule $eventSchedule
+         *
+         * @ParamConverter("event", options={"mapping": {"event_id": "id"}})
+         * @ParamConverter("eventSchedule", options={"mapping": {"schedule_id": "id"}})
+         *
+         * @return Response
+         */
+        public function calendar(Event $event, EventSchedule $eventSchedule)
+        {
+            $filename = 'event.ics';
+            $eventStartDate = date('Ymd\THis', strtotime($event->getStartedAt()->format('Ymd\T') . $eventSchedule->getTime()->format('Hi')));
+
+            $content = $this->renderView('event/__ics.html.twig', [
+                'event' => $event,
+                'currentDate' => date('Ymd\THis'),
+                'eventStartDate' => $eventStartDate,
+                'uniqid' => uniqid()
+            ]);
+
+            $response = new Response($content);
+            $disposition = $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $filename);
+
+            $response->headers->set('Content-Disposition', $disposition);
+            $response->headers->set('Content-type', 'text/plain');
+
+            return $response;
         }
     }
